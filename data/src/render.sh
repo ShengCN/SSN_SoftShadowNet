f125fbc96d2ea83007ecdeeeb8a5c719487dbd31
yichen sheng <sheng30@purdue.edu>
2022-06-25 23:57:39 -0400

    Add pytorch dataset.

--- data/src/render.sh
+++ data/src/render.sh
@@ -7,8 +7,8 @@ start_id=0
 end_id=-1
 width=512
 height=512
-out_hdf5="./base/all_base.hdf5"
+out_hdf5="./base/dataset.hdf5"
 
-#python render.py --cpus=4 --gpus=0 --ds_root=$ds_root --out_folder=cache --start_id=$start_id --end_id=$end_id --width=$width --height=$height --cam_pitch 15 30 45 --model_rot -45 0 45
+# python render.py --cpus=4 --gpus=0 --ds_root=$ds_root --out_folder=$cache --start_id=$start_id --end_id=$end_id --width=$width --height=$height --cam_pitch 15 --model_rot 0
 
 python build_hdf5.py --cache=$cache --width=$width --height=$height --out_hdf5=$out_hdf5
